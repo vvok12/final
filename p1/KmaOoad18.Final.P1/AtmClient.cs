5248cc205129e5736f17fad90026aca81b03a2e4
vvok12 <v.v.bezshtanko@gmail.com>
2018-12-07 08:22:54 +0200

    p1 done

--- p1/KmaOoad18.Final.P1/AtmClient.cs
+++ p1/KmaOoad18.Final.P1/AtmClient.cs
@@ -4,24 +4,40 @@ namespace KmaOoad18.Final.P1
 {
     public class AtmClient
     {
+        private Dictionary<int, int> banknotesAmountPairs = new Dictionary<int, int>();
+        
         // What banknotes ATM has at the moment, e.g. [50, 200, 500] etc
         public List<int> AvailableBanknotes()
         {
-            return new List<int>();
+            List<int> ava = new List<int>();
+            foreach (int key in banknotesAmountPairs.Keys)
+                if (banknotesAmountPairs[key] > 0)
+                    ava.Add(key);
+            ava.Sort();
+
+            return ava;
+            //return new List<int>();
         }
 
         // How much money ATM has
         public long AvailableAmount()
         {
-            return -1;
+            long sum = 0;
+            foreach (int key in banknotesAmountPairs.Keys)
+                sum += banknotesAmountPairs[key] * key;
+
+            return sum;
         }
 
         // Input is list of pairs - banknote and its quantity, e.g. (100, 1000), (200, 500) etc,
         // that means we load ATM with 1000 of 100 banknotes and 500 of 200 banknotes
         public AtmClient Refill(params (int banknote, int qty)[] cash)
         {
-            // Refill ATM with given cash amount
-
+            //banknotesAmountPairs = new Dictionary<int, int>();
+            //List<int> banks = AvailableBanknotes();
+            foreach ((int b, int q) in cash)
+                banknotesAmountPairs[b] = q;
+            
             return this;
         }
 
@@ -29,8 +45,24 @@ namespace KmaOoad18.Final.P1
         // Check that ATM has enough banknotes to give that amount
         public AtmClient Withdraw(long amount)
         {
-            // Update ATM 
 
+            // Update ATM 
+            if (AvailableAmount() >= amount)
+            {
+                List<int> ava = AvailableBanknotes();
+                ava.Reverse();
+                foreach (int banknoteType in ava)
+                {
+                    long toWith = System.Math.Min(
+                        banknotesAmountPairs[banknoteType], amount / banknoteType
+                        );
+                    banknotesAmountPairs[banknoteType] -= (int)toWith;
+                    amount -= banknoteType * toWith;
+                    if (amount == 0) break;
+                }
+            }
+            //else throw new System.Exception("ATM has not enough money to withdrow");
+            
             return this;
         }
     }
